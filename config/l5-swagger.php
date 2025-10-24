498a7c307f265a1019e9dd3ae37a9d33a57b2a80
Mame_diarra_bousso <kamamediarra2002@gmai.com>
2025-10-24 03:49:43 +0000

    swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/v1/docs',
+                'api' => 'api/documentation',
             ],
             'paths' => [
                 /*
